bd8331de6a56067b6a9c7744e27d23273a134cdf
VolVla <98841869+VolVla@users.noreply.github.com>
2022-02-01 21:18:29 +0300

    Add files via upload

---
+++ HomeWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int coins;
+            decimal upPoints;
+            float speedRun;
+            string stringText;
+            bool isOpenDoor;
+            string inputName;
+            int numberColumn;
+            long anyNumber;
+            double bonusExperience;
+            char secretChar;
+            byte heightOfBlocksMinecraft;
+            bool canAttackEnemy;
+
+        }
+    }
+}
