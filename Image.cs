b29a8b5cd41ccdbbecad91481b89ad47f7b6a23f
VolVla <98841869+VolVla@users.noreply.github.com>
2022-02-05 17:37:31 +0300

    Add files via upload

---
+++ Image.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Image
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfImages = 52;
+            int capacityRow = 3;
+            int filledOfRow;
+            int imageBeyondMeasure;
+            filledOfRow = numberOfImages / capacityRow;
+            imageBeyondMeasure = numberOfImages % capacityRow;
+            Console.WriteLine("У вас " + filledOfRow + " полностью заполненных рядов ,и " + imageBeyondMeasure + " картинка сверх меры");
+
+        }
+    }
+}
