804c2a6e77ee05b60b126876aae6addd37b5511d
VolVla <98841869+VolVla@users.noreply.github.com>
2022-02-05 15:51:42 +0300

    Add files via upload

---
+++ Hospital.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int timeService = 10;
+            int time, min, hour, humans;
+            
+            Console.Write("Введите кол-во людей в очереди:");
+            humans = Convert.ToInt32(Console.ReadLine());
+            time = humans * timeService;
+            hour = time / 60;
+            min = time % 60;
+            Console.WriteLine("Вы должны отстоять в очереди: " + hour + " часа " + min + " минут.");
+
+        }
+    }
+}
