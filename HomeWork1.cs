7433fcba9f326b2a2883f377078eefb01eab91fc
VolVla <98841869+VolVla@users.noreply.github.com>
2022-02-05 16:02:54 +0300

    Add files via upload

---
+++ HomeWork1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameUser;
+            string zodiacSign;
+            Console.WriteLine("Как вас зовут");
+            nameUser = Console.ReadLine();
+            Console.WriteLine("Какой ваш знак зодиака");
+            zodiacSign = Console.ReadLine();
+            Console.WriteLine("Вас зовут " + nameUser + ",вам 21 год, вы " + zodiacSign + " работаете на заводе." );
+        }
+    }
+}
