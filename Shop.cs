f0a501b872127a5939cc1a384652e2cf0ef08a5c
VolVla <98841869+VolVla@users.noreply.github.com>
2022-02-05 15:32:44 +0300

    Add files via upload

---
+++ Shop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int goldPlayer;
+            int crystalPlayer = 0;
+            int priceCristal = 2;
+            int volumeBuyCrystal;
+
+            
+            Console.Write("Введите начально кол-во золота:");
+            goldPlayer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Добрый день. Добро пожаловать в мой магазин. Один Кристал стоит " + priceCristal + " золота.");
+            Console.WriteLine("Cколько кристаллов вы хотите купить?");
+            volumeBuyCrystal = Convert.ToInt32(Console.ReadLine());
+
+            goldPlayer = goldPlayer - (volumeBuyCrystal * priceCristal);
+            crystalPlayer += volumeBuyCrystal;
+            Console.WriteLine("Вы купили: " + volumeBuyCrystal + " кристалов по цене:" + priceCristal + " золота.");
+            Console.WriteLine("У вас в рюкзаке: " + goldPlayer + " золота и " + crystalPlayer + " кристалов." );
+
+        }
+    }
+}
