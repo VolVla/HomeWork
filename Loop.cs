5726e122a7f425f558b4e10f6a3cfa603827b154
VolVla <98841869+VolVla@users.noreply.github.com>
2022-02-06 18:39:00 +0300

    Add files via upload

---
+++ Loop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loop = 1;
+            string inputOfMessage;
+            while (loop > 0) { 
+            Console.WriteLine("Введите слово exit.");
+            inputOfMessage = Console.ReadLine();
+            if (inputOfMessage == "exit")
+                 {
+                    Console.WriteLine("Вы вышли из цикла.");
+                    --loop; 
+                 }
+            }
+        }
+    }
+}
