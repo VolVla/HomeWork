2a9b7c7745e041c85a76e6fa191cc97ed16603d3
VolVla <98841869+VolVla@users.noreply.github.com>
2022-02-09 22:28:02 +0300

    Add files via upload

---
+++ Cycle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int numberOnIncrease = 7;
+            int lenghtArray = 14;
+            int numberExit = 7;
+            
+            for(int i=0; i < lenghtArray; i++)
+            {
+                Console.WriteLine(numberExit);
+                numberExit += numberOnIncrease;
+            }
+        }
+    }
+}
