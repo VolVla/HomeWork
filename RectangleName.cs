176540b228291ccdeaec18b604144f6a4d233700
VolVla <98841869+VolVla@users.noreply.github.com>
2022-02-12 23:30:47 +0300

    Update RectangleName.cs

--- RectangleName.cs
+++ RectangleName.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,34 +10,31 @@ namespace RectangleName
     {
         static void Main(string[] args)
         {
-            int heightPositionLine = 6;
-            int indentTextName = 1;
-            int valueRow = 3;
             const int ValueInitial = 0;
+            int heightPositionTextName = 6;
+            int widthPositionTextName = 1;
+            int valueRow = 3;
+            int cursorPosition = 8;
             char symbol;
             string userName;
-            bool isExit = false;
 
-            while (isExit == false)
+            Console.WriteLine("Введите имя пользователя");
+            userName = Console.ReadLine();
+            Console.WriteLine("Введите любой символ");
+            symbol = Convert.ToChar(Console.ReadLine());
+
+            for (int y = ValueInitial; y <= valueRow; y++)
             {
-                Console.WriteLine("Введите имя пользователя");
-                userName = Console.ReadLine();
-                Console.WriteLine("Введите любой символ");
-                symbol = Convert.ToChar(Console.ReadLine());
-                
-                for (int y = ValueInitial; y < valueRow; y++)
-                {
-                    Console.WriteLine(symbol);
+                Console.WriteLine(symbol);
 
-                        for (int x = ValueInitial; x < userName.Length + indentTextName; x++)
-                        {
-                            Console.Write(symbol);
-                        }
+                for (int x = ValueInitial; x <= userName.Length; x++)
+                {
+                    Console.Write(symbol);
                 }
-                Console.SetCursorPosition(indentTextName, heightPositionLine);
-                Console.Write(userName);
-                isExit = true;
             }
+                Console.SetCursorPosition(widthPositionTextName, heightPositionTextName);
+                Console.Write(userName);
+                Console.SetCursorPosition(ValueInitial, cursorPosition);
         }
     }
 }
