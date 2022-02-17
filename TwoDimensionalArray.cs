38dc046ad27307aeea20b09e7a30dc746886310d
VolVla <98841869+VolVla@users.noreply.github.com>
2022-02-17 22:17:54 +0300

    Add files via upload

---
+++ TwoDimensionalArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arrayLine = { { 1,2,3 }, { 5,7,2 } };
+            int sumLine2 = 0 ;
+            int productColumn1 = 0;
+            int result;
+            
+            for (int y = 0; y < arrayLine.GetLength(0); y++)
+            {
+                sumLine2 = arrayLine[1,0] + arrayLine[1,1] + arrayLine[1,2];
+                
+                for (int x = 0; x < arrayLine.GetLength(1);x++ )
+                {
+                    productColumn1 = arrayLine[0, 0] * arrayLine[0, 1] * arrayLine[0, 2];
+                    Console.Write(arrayLine[y, x] + " ");
+                }
+                Console.WriteLine();
+            }
+            result = sumLine2 + productColumn1;
+            Console.WriteLine(result);
+        }
+    }
+}
