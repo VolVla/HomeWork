866372e8ddaea2e3c436d130a1d05dfd5a11bc52
VolVla <98841869+VolVla@users.noreply.github.com>
2022-02-21 20:58:29 +0300

    Update TwoDimensionalArray.cs

--- TwoDimensionalArray.cs
+++ TwoDimensionalArray.cs
@@ -10,35 +10,32 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            int[,] arrayLine = { { 2,2,3 }, { 5,7,2 } };
-            int sumLine2 = 0;
-            int productColumn1 = 1;
-            int result;
+            int[,] arrayLine = { { 2,2,3,4 }, { 5,7,2,1 } };
+            int sumLine = 0;
+            int productColumn = 1;
             int numberLine = 1;
             int lineArray = 0;
             int columnArray = 0;
-            int initialValueArray = 0;
-
-            for (int y = initialValueArray; y < arrayLine.GetLength(1); y++)
+             
+            for (int i = 0; i < arrayLine.GetLength(1); i++)
             {
-              sumLine2 += arrayLine[numberLine, initialValueArray];
+                sumLine += arrayLine[numberLine,i ];
             }
 
-            for (int x = initialValueArray; x < arrayLine.GetLength(0); x++)
+            for (int j = 0; j < arrayLine.GetLength(0); j++ )
             {
-              productColumn1 *= arrayLine[lineArray++, columnArray];
+              productColumn *= arrayLine[lineArray++, columnArray];
             }
             
-            for (int i = initialValueArray; i < arrayLine.GetLength(0); i++)
+            for (int i = 0; i < arrayLine.GetLength(0); i++)
             {
-                for (int j = initialValueArray; j < arrayLine.GetLength(1); j++)
+                for (int j = 0; j < arrayLine.GetLength(1); j++)
                 {
-                  Console.Write(arrayLine[i, j] + " ");
+                    Console.Write(arrayLine[i, j] + " ");
                 }
                 Console.WriteLine();
             } 
-            result = sumLine2 + productColumn1;
-            Console.WriteLine(result);
+            Console.WriteLine(sumLine +" "+productColumn);
         }
     }
 }
