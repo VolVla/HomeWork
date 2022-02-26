ef04bfaa8c3bd8cdc8fa028b78a8250ab13acda1
VolVla <98841869+VolVla@users.noreply.github.com>
2022-02-26 23:44:23 +0300

    Add files via upload

---
+++ OneArray.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayElement = new int[30];
+            int leftValue = 0;
+            int rightValue = 0;
+            int number = 0;
+            int maximum = 0;
+            Random random = new Random();
+
+            for(int i = 0; i < arrayElement.Length; i++)
+            {
+                arrayElement[i] = random.Next(1, 10);
+                Console.Write(arrayElement[i] + " ");
+            }
+            Console.WriteLine("$");
+
+            for(int i = 1; i < arrayElement.Length - 1  ; i++)
+            {
+               
+                
+
+               
+                
+                    
+                    if (arrayElement[i-1] < arrayElement[i] && arrayElement[i + 1]  < arrayElement[i])
+                    {
+                    Console.Write(arrayElement[i] + " ");
+                     
+                    
+                    }
+               
+                
+                
+                
+                
+                
+                 
+            }
+        }
+    }
+}
