11205ccc9a9821786bf351dae052ca1ae8ce8f82
VolVla <98841869+VolVla@users.noreply.github.com>
2022-03-07 12:38:08 +0300

    Update SumEnteredNumbers.cs

--- SumEnteredNumbers.cs
+++ SumEnteredNumbers.cs
@@ -10,51 +10,48 @@ namespace SumEnteredNumbers
     {
         static void Main(string[] args)
         {
-            int enterNumber;
-            int sizeArray = 0;
-            int[] arrayNumbers = new int[sizeArray];
+            int[] arrayNumbers = new int[0];
             bool isExit = false;
             string userInput;
-            string const WordExit = "exit";
-            string const CommandAmount = "sum";
-
-            Console.WriteLine("Введите числа через пробел");
+            string wordExit = "exit";
+            string commandAmount = "sum";
+            
             while(isExit == false)
             {
-                Console.WriteLine("\nКомманды \n1)Для ввода числа напишите в чат " + wordEnterNumbers + "\n2)Для вывода суммы всех введеных чисел.Введите слово "+ wordAmount +"\n3)Для выхода из программы.Введите слово "+ wordExit);
-                userInput = Console.ReadLine();
-
-                switch (userInput)
+                Console.WriteLine("Введите число\nДля вывода суммы всех введеных чисел.Введите слово "+ commandAmount + "\nДля выхода из программы.Введите слово "+ wordExit);
+                userInput = Console.ReadLine(); 
+                
+                if (userInput == wordExit)
                 {
-                    case userInput:
-                        enterNumber = Convert.ToInt32 (userInput);
-                        int[] tempArrayNumbers = new int [arrayNumbers.Length + 1];
-                        tempArrayNumbers[tempArrayNumbers.Length - 1] = enterNumber;
+                    Console.WriteLine("Вы вышли из программы.");
+                    isExit = true;
+                }
+                else if (userInput == commandAmount)
+                {
+                    int sumNumbers = 0;
 
-                        for (int i = 0; i < arrayNumbers.Length; i++)
-                        {
-                            tempArrayNumbers[i] = arrayNumbers[i];
-                        }
-                        arrayNumbers = tempArrayNumbers;
-                        sizeArray++;
-                        break;
-                        
-                    case CommandAmount:
-                        int sumNumbers = 0;
+                    for (int i = 0; i < arrayNumbers.Length; i++)
+                    {
+                        sumNumbers += arrayNumbers[i] ;
+                    }
 
-                        for (int i = 0; i < arrayNumbers.Length; i++)
-                        {
-                            sumNumbers += arrayNumbers[i] ;
-                        }
-                        Console.WriteLine(sumNumbers); 
-                        
-                        break;
-                        
-                    case WordExit:
-                        Console.WriteLine("Вы вышли из программы.");
-                        isExit = true;
-                        break;
-                } 
+                    Console.WriteLine(sumNumbers);    
+                }
+                else 
+                {
+                    Console.WriteLine("\n1)Для ввода числа напишите в чат");
+                    int enterNumber;
+                    enterNumber = Convert.ToInt32(userInput);
+                    int[] tempArrayNumbers = new int [arrayNumbers.Length + 1];
+                    tempArrayNumbers[tempArrayNumbers.Length - 1] = enterNumber;
+
+                    for (int i = 0; i < arrayNumbers.Length; i++)
+                    {
+                        tempArrayNumbers[i] = arrayNumbers[i];
+                    }
+
+                    arrayNumbers = tempArrayNumbers;
+                }
             }
         }
     }
