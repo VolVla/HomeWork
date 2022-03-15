c96858826a3b2bb75635d12908e5d26ea48a3f5b
VolVla <98841869+VolVla@users.noreply.github.com>
2022-03-15 19:57:28 +0300

    Add files via upload

---
+++ TwoVarriables.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoVarriables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string namePerson = "Сергеевич";
+            string surName = "Александр";
+            string tempValue;
+
+            Console.Write($"До перестановки имя {namePerson}, фамилия {surName} ");
+            tempValue = surName;
+            surName = namePerson;
+            namePerson = tempValue;
+            Console.Write($"\nДо перестановки имя {namePerson}, фамилия {surName} ");
+        }
+    }
+}
