3d0ae64d91a8ed169db9a6fdcf4eae9033468efe
VolVla <98841869+VolVla@users.noreply.github.com>
2022-03-19 12:41:02 +0300

    Add files via upload

---
+++ SortingBubble.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingArrayElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10] { 4, 6, 8, 1, 2, 3, 7, 9, 0, 5 };
+
+            for (int i = 0;  i < array.Length; i++)
+            {
+                for(int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j+1])
+                    { 
+                        int a = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = a;
+                    }
+                }
+            }
+
+            foreach (int number in array)
+            {
+                Console.Write(number);
+            }
+        }
+    }
+}
