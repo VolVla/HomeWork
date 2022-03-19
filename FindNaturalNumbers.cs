5d4b7b53c416b5acb1d307283f5f2fb2b07a6821
VolVla <98841869+VolVla@users.noreply.github.com>
2022-03-19 14:30:00 +0300

    Add files via upload

---
+++ FindNaturalNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindNaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maximumValue = 28;
+            int minimumValue = 1;
+            int number = 0;
+            Random random = new Random();
+            int valueElementN = random.Next(minimumValue, maximumValue);
+
+            for(int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        if (i + j + k == valueElementN)
+                        {
+                            number++;
+                            Console.Write(i * 100 + j * 10 + k +" ");
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"\nЧисло N = {valueElementN} количество трехзначных натуральных чисел {number}");
+        }
+    }
+}
