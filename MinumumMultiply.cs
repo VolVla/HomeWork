bf2447a97187a31b0f859e49353000fff7a3690a
VolVla <98841869+VolVla@users.noreply.github.com>
2022-04-02 10:41:13 +0300

    Add files via upload

---
+++ MinumumMultiply.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinumumMultiply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int minumumValueNumber = 2;
+            int maxumumValueNumber = 10;
+            int degree = 0;
+            int superiorNumber = 1;
+            int numberDegree = 2;
+            Random random = new Random();
+            number = random.Next(minumumValueNumber, maxumumValueNumber);
+
+            for(int i = 0; superiorNumber <= number; i++)
+            {
+                superiorNumber *= numberDegree;
+                degree++;
+            }
+            Console.WriteLine($"Число {number}, степень {degree}, само число превосходящая заданое число {superiorNumber}");
+        }
+    }
+}
