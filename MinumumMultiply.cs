0def51f7397fc962060ae3f0f8b863e654e59d31
VolVla <98841869+VolVla@users.noreply.github.com>
2022-04-02 12:07:42 +0300

    Update MinumumMultiply.cs

--- MinumumMultiply.cs
+++ MinumumMultiply.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,7 +19,7 @@ namespace MinumumMultiply
             Random random = new Random();
             number = random.Next(minumumValueNumber, maxumumValueNumber);
 
-            for(int i = 0; superiorNumber <= number; i++)
+            while(superiorNumber<=number)
             {
                 superiorNumber *= numberDegree;
                 degree++;
