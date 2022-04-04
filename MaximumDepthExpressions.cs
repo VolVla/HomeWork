cc5af6b6ec76d712cfc3f6fef10d53f2d20b493a
VolVla <98841869+VolVla@users.noreply.github.com>
2022-04-04 20:46:17 +0300

    Add files via upload

---
+++ MaximumDepthExpressions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MaximumDepthExpressions
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int maximumDepthParentheses = 0; 
+            int valueIncorrectParentheses = 0;
+            char leftParenthesis = '(';
+            char rightParenthesis = ')';
+            string text = "(()())";
+
+            foreach ( char symbols  in text) { 
+                if (symbols == leftParenthesis){
+                    valueIncorrectParentheses++;
+                }
+                else if (symbols == rightParenthesis){
+                    maximumDepthParentheses++;
+                    valueIncorrectParentheses--;
+                }
+            }
+
+            if (valueIncorrectParentheses != 0){
+                Console.WriteLine($"Не корректное скобочное выражение.");
+            }
+            else {
+                Console.WriteLine($"Корректное скобочное выражение, максимальная глубина вложенных скобок {maximumDepthParentheses}");
+            } 
+        }
+    }
+}
