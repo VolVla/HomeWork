711844dd3525e8e85d723fa58bc714fa0407d598
VolVla <98841869+VolVla@users.noreply.github.com>
2022-04-14 20:18:13 +0300

    Add files via upload

---
+++ ArrayWord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text ="Дана строка с текстом используя метод строки ";
+            string[] arrayWords = text.Split(' ');
+
+            foreach (string word in arrayWords){
+                Console.WriteLine($"{word}");
+            }
+        }
+    }
+}
