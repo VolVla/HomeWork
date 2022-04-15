c37407d13524c51c93db458d33f4ead069871d61
VolVla <98841869+VolVla@users.noreply.github.com>
2022-04-15 22:30:14 +0300

    Add files via upload

---
+++ ArrayShift.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int AmountArray = 5;
+            int[] arrayNumbers = new int[AmountArray] {1,2,3,4,5};
+            int valueShiftLeft;
+
+            valueShiftLeft = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = arrayNumbers.Length; i > valueShiftLeft; --i){
+                int aLast = arrayNumbers[arrayNumbers.Length - 1];
+
+                for (int j = arrayNumbers.Length - 1; j > 0; j--) {
+                arrayNumbers[j] = arrayNumbers[j - 1];
+                }    
+                arrayNumbers[0] = aLast;
+            }
+
+            foreach (int number in arrayNumbers){
+                Console.WriteLine($" {number}");
+            }
+        }
+    }
+}
