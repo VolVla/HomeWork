d04a0ff8e730c784d3311b4d7be95b2217c8ae96
VolVla <98841869+VolVla@users.noreply.github.com>
2022-04-16 11:43:16 +0300

    Update LenghtSubArray.cs

--- LenghtSubArray.cs
+++ LenghtSubArray.cs
@@ -13,7 +13,7 @@ namespace LenghtSubArray
            const int AmountNumbers = 30;
            int[] arrayNumbers = new int[AmountNumbers] { 2, 3, 2, 2, 4, 5, 4, 6, 7, 7, 7, 7, 7, 7, 2, 1, 3, 1, 1, 1, 6, 1, 4, 1, 1, 1, 2, 2, 3, 3};
            int maximumValue = arrayNumbers[0];
-           int numberRepeat = 0;
+           int numberRepeat = 1;
 
            for(int i = 0;i < arrayNumbers.Length - 1;i++)
            { 
