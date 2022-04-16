c0081026a14f4490282c10862e693cf5a917f3fd
VolVla <98841869+VolVla@users.noreply.github.com>
2022-04-16 11:17:15 +0300

    Update LenghtSubArray.cs

--- LenghtSubArray.cs
+++ LenghtSubArray.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,26 +11,33 @@ namespace LenghtSubArray
         static void Main(string[] args)
         {
            const int AmountNumbers = 30;
-           int[] arrayNumbers = new int[AmountNumbers] { 2, 3, 2, 2, 4, 5, 4, 6, 7, 7, 7, 7, 7, 1, 2, 1, 3, 1, 1, 1, 6, 1, 4, 1, 1, 1, 2, 2, 3, 3};
+           int[] arrayNumbers = new int[AmountNumbers] { 2, 3, 2, 2, 4, 5, 4, 6, 7, 7, 7, 7, 7, 7, 2, 1, 3, 1, 1, 1, 6, 1, 4, 1, 1, 1, 2, 2, 3, 3};
            int maximumValue = arrayNumbers[0];
            int numberRepeat = 0;
-           int sequenceNumbers = 3;
 
-           for(int i = 0; i < arrayNumbers.Length - sequenceNumbers; i++){
-                int value = 0;
-                for (int j = 0; j < i + sequenceNumbers; j++){
-                    value = arrayNumbers[i];
+           for(int i = 0;i < arrayNumbers.Length - 1;i++)
+           { 
+                int values; 
+                values = arrayNumbers[i];
+
+                if(arrayNumbers[i] == arrayNumbers[i+1])
+                {
+                    if (maximumValue < values)
+                    {
+                        maximumValue = values;
+                    }
+                    else if (maximumValue == values)
+                    {
+                        numberRepeat++; 
+                    }
                 }
-                if (maximumValue < value){
-                    maximumValue = value;
-                    numberRepeat++;
-                } 
            }
-            Console.WriteLine($"\nСамое большое число {maximumValue}, кол-во повторений {numberRepeat}");
+           Console.WriteLine($"\nСамое большое число {maximumValue}, кол-во повторений {numberRepeat}");
 
-            foreach (int value in arrayNumbers){
+           foreach (int value in arrayNumbers)
+           {
                Console.Write($"{value} ");
-            } 
+           } 
         }
     }
 }
