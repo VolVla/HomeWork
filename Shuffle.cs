76713c8a7db34342fad271b1d97dc2f154017163
VolVla <98841869+VolVla@users.noreply.github.com>
2022-04-18 21:53:22 +0300

    Add files via upload

---
+++ Shuffle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int AmountNumbers = 5;
+            int[] array = new int[AmountNumbers] { 1, 2, 3, 4, 5 };
+            Shuffle(array);
+            Foreach(array);
+        }
+
+        static void Shuffle(int[] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Swap(array, i, i + random.Next(array.Length - i));
+            }
+        }
+        static void Swap(int[] array, int a, int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+        static void Foreach(int[] array)
+        {
+            foreach (int value in array)
+            {
+                Console.Write($"{value} ");
+            }
+        }
+    }
+}
