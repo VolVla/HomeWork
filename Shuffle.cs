531ca1792213eb65ec49679eb08ddcde9ae42c85
VolVla <98841869+VolVla@users.noreply.github.com>
2022-04-21 20:55:13 +0300

    Update Shuffle.cs

--- Shuffle.cs
+++ Shuffle.cs
@@ -8,36 +8,26 @@ namespace Shuffle
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
             const int AmountNumbers = 5;
-            int[] array = new int[AmountNumbers] { 1, 2, 3, 4, 5 };
+            int[] array = new int[AmountNumbers] { 1, 2, 3, 4, 5};
             Shuffle(array);
-            ForeachArray(array);
-        }
+            OutputArray(array);}
 
-        static void Shuffle(int[] array)
-        {
+        static void Shuffle(int[] array){
             Random random = new Random();
             int lenghtArray = array.Length;
 
-            for (int i = 0; i < lenghtArray; i++)
-            {
-                Swap(ref array, i, random.Next(lenghtArray));
-            }
-        }
-        static void Swap(ref int[] array, int firstNumber , int secondNumber)
-        {
+            for (int i = 0; i < lenghtArray; i++){
+                Swap(array, i, random.Next(lenghtArray));}}
+
+        static void Swap(int[] array, int firstNumber, int secondNumber){
             int temporaryValue = array[firstNumber];
             array[firstNumber] = array[secondNumber];
-            array[secondNumber] = temporaryValue;
-        }
-        static void ForeachArray(int[] array)
-        {
-            foreach(int value in array)
-            {
-                Console.Write($"{value} ");
-            }
-        }
+            array[secondNumber] = temporaryValue;}
+
+        static void OutputArray(int[] array){
+            foreach(int numbers in array){
+                Console.Write($"{numbers} ");}}
     }
 }
