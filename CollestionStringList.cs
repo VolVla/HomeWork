5380ca102ff18648e5943875243aa78f81b62110
VolVla <98841869+VolVla@users.noreply.github.com>
2022-06-23 23:04:42 +0300

    Add files via upload

---
+++ CollestionStringList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = new int[] { 1, 2, 1 };
+            int[] secondArray = new int[] { 3, 2, 6 };
+            List<int> arrayList = new List<int>();
+
+            AddArrayInCollection(arrayList, firstArray);
+            AddArrayInCollection(arrayList, secondArray);
+
+            foreach (int element in arrayList)
+            {
+                Console.WriteLine($"{element}");
+            }
+        }
+
+        static void AddArrayInCollection(List<int> arrayList,int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (arrayList.Contains(array[i]) == false)
+                {
+                    arrayList.Add(array[i]);
+                }
+            }
+        }
+    }
+}
