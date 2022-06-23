77e389997a9125e03f6412016bc972ba977b1c2d
VolVla <98841869+VolVla@users.noreply.github.com>
2022-06-23 12:15:51 +0300

    Add files via upload

---
+++ QueueTheStore.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueTheStore
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isExit = false;
+            int moneyAccount = 0;
+            Queue<int>  amountBuy= new Queue<int>();
+            amountBuy.Enqueue(100);
+            amountBuy.Enqueue(20);
+            amountBuy.Enqueue(600);
+            amountBuy.Enqueue(90);
+            amountBuy.Enqueue(10);
+            amountBuy.Enqueue(60);
+            amountBuy.Enqueue(90);
+           
+            while (isExit == false)
+            {
+                if (amountBuy.Count > 0)
+                {
+                    Console.WriteLine("Вы обслуживаете покупателя");
+                    AddMoneyAccount(amountBuy, ref moneyAccount);
+                    Console.WriteLine("Нажмите любую кнопку");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Вы закончили обслуживать посетителей");
+                    isExit = true;
+                }
+            }
+        }
+
+        static void AddMoneyAccount(Queue<int> number,ref int moneyAccount)
+        {
+            moneyAccount += number.Dequeue();
+            Console.WriteLine($"Вы обслужили клиента ваш счет {moneyAccount}");
+        }
+    }
+}
