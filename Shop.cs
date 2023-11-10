6601557fb4f4481b9c287d1abcdc162be5419012
VolVla <98841869+VolVla@users.noreply.github.com>
2023-11-10 14:18:43 +0300

    Update Shop.cs

--- Shop.cs
+++ Shop.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,20 +7,18 @@ namespace Shop
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             int goldPlayer;
             int crystalPlayer = 0;
             int priceCristal = 2;
             int volumeBuyCrystal;
 
-            
             Console.Write("Введите начально кол-во золота:");
             goldPlayer = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Добрый день. Добро пожаловать в мой магазин. Один Кристал стоит " + priceCristal + " золота.");
             Console.WriteLine("Cколько кристаллов вы хотите купить?");
             volumeBuyCrystal = Convert.ToInt32(Console.ReadLine());
-
             goldPlayer = goldPlayer - (volumeBuyCrystal * priceCristal);
             crystalPlayer += volumeBuyCrystal;
             Console.WriteLine("Вы купили: " + volumeBuyCrystal + " кристалов по цене:" + priceCristal + " золота.");
