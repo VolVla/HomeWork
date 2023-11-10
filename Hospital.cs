08e5287b4cc70f4989a67a662a568bd01fc64ee3
VolVla <98841869+VolVla@users.noreply.github.com>
2023-11-10 21:33:34 +0300

    Update Hospital.cs

--- Hospital.cs
+++ Hospital.cs
@@ -6,15 +6,15 @@ namespace Hospital
     {
         static void Main(string[] args)
         {
-            const int timeMinuteOfHour = 60;
-            const int timeService = 10;
+            int timeMinuteOfHour = 60;
+            int timeService = 10;
             int allWaitingTime;
             int waitingTimeMinute;
             int waitingTimeHour;
             
             Console.Write("Введите кол-во людей в очереди:");
             int.TryParse(Console.ReadLine(), out int numberHumans);
-            allWaitingTime = numberHumans * timeService;
+            allWaitingTimes = numberHumans * timeService;
             waitingTimeHour = allWaitingTime / timeMinuteOfHour;
             waitingTimeMinute = allWaitingTime % timeMinuteOfHour;
             Console.WriteLine($"Вы должны отстоять в очереди: {waitingTimeHour} часа {waitingTimeMinute} минут.");
