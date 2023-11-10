de7fc146277b31a0acb589f3812c5eece04c3f3e
VolVla <98841869+VolVla@users.noreply.github.com>
2023-11-10 15:38:41 +0300

    Update SimpleProgram.cs

--- SimpleProgram.cs
+++ SimpleProgram.cs
@@ -1,23 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace SimpleProgram
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            const string CommandSetColorText = "1";
+            const string CommandSetColorFonConsole = "2";
+            const string CommandSetConsoleSize = "3";
+            const string CommandSetWordOfWelcome = "4";
+            const string CommandSetPositionCursor = "5";
+            const string CommandClearConsole = "6";
+            const string CommandExitProgram = "exit";
+            const string CommandSetRed = "1";
+            const string CommandSetGreen = "2";
+            const string CommandSetBlue = "3";
+            const string CommandSetYellow = "4";
             bool isExit = false;
-            string colorText;
-            string colorFonConsole;
             string userInput;
-            string exitText;
-            string clearText;
-            string wordClear = "clear";
-            string wordExit = "exit";
             string wordsWelcome = "Добро пожаловать в мою программу.";
             int sizeHeightConsole;
             int sizeWidthConsole;
@@ -26,89 +27,80 @@ namespace SimpleProgram
 
             while (isExit == false)
             {
-                Console.WriteLine( wordsWelcome + "\nЗдесь вы можете. \n 1 - Установить цвет текста.\n 2 - Установить цвет фона консоли.\n 3 - Установить размер окна консоли.\n 4 - Установить слова приветсвия в программе.\n 5 - Установить позицию курсора в консоли. \n 6 - Очистить консоль.\n 7 - Выход из программы.");
-                    userInput = Console.ReadLine();
+                Console.WriteLine(wordsWelcome + "\nЗдесь вы можете. \n 1 - Установить цвет текста.\n 2 - Установить цвет фона консоли.\n 3 - Установить размер окна консоли.\n 4 - Установить слова приветсвия в программе.\n 5 - Установить позицию курсора в консоли. \n 6 - Очистить консоль.\n 7 - Выход из программы.");
+                userInput = Console.ReadLine();
 
                 switch (userInput)
                 {
-                    case "1":
+                    case CommandSetColorText:
                         Console.WriteLine("\n 1 - Установить красный  цвет текста.\n 2 - Установить зелёный  цвет текста.\n 3 - Установить жёлтый  цвет текста.\n 4 - Установить синий  цвет текста. ");
-                            colorText = Console.ReadLine();
 
-                        switch (colorText)
+                        switch (Console.ReadLine())
                         {
-                            case "1":
+                            case CommandSetRed:
                                 Console.ForegroundColor = ConsoleColor.Red;
                                 break;
-                            case "2":
+                            case CommandSetGreen:
                                 Console.ForegroundColor = ConsoleColor.Green;
                                 break;
-                            case "3":
+                            case CommandSetYellow:
                                 Console.ForegroundColor = ConsoleColor.Yellow;
                                 break;
-                            case "4":
+                            case CommandSetBlue:
                                 Console.ForegroundColor = ConsoleColor.Blue;
-                                break;   
+                                break;
                         }
                         break;
-                    case "2":
+                    case CommandSetColorFonConsole:
                         Console.WriteLine("\n 1 - Установить красный  цвет фона.\n 2 - Установить зелёный  цвет фона.\n 3 - Установить жёлтый  цвет фона.\n 4 - Установить синий  цвет фона. ");
-                        colorFonConsole = Console.ReadLine();
 
-                        switch (colorFonConsole)
+                        switch (Console.ReadLine())
                         {
-                            case "1":
+                            case CommandSetRed:
                                 Console.BackgroundColor = ConsoleColor.Red;
                                 break;
-                            case "2":
+                            case CommandSetGreen:
                                 Console.BackgroundColor = ConsoleColor.Green;
                                 break;
-                            case "3":
+                            case CommandSetYellow:
                                 Console.BackgroundColor = ConsoleColor.Yellow;
                                 break;
-                            case "4":
+                            case CommandSetBlue:
                                 Console.BackgroundColor = ConsoleColor.Blue;
                                 break;
                         }
                         break;
-                    case "3":
+                    case CommandSetConsoleSize:
                         Console.WriteLine(" Предупреждение.Размер консоли ограничен до 63.\nУстановите размер высоты окна консоли.");
-                            sizeHeightConsole = Convert.ToInt32(Console.ReadLine());
+                        sizeHeightConsole = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine(" Установите размер ширины окна консоли.");
-                            sizeWidthConsole = Convert.ToInt32(Console.ReadLine());
+                        sizeWidthConsole = Convert.ToInt32(Console.ReadLine());
                         Console.WindowHeight = sizeHeightConsole;
                         Console.WindowWidth = sizeWidthConsole;
                         break;
-                    case "4":
+                    case CommandSetWordOfWelcome:
                         Console.WriteLine("Введите текст приветствия в программе.");
-                            wordsWelcome = Console.ReadLine();
+                        wordsWelcome = Console.ReadLine();
                         break;
-                    case "5":
+                    case CommandSetPositionCursor:
                         Console.WriteLine(" Установите положение высоты позиции курсора.");
-                            heightPositionCursor = Convert.ToInt32(Console.ReadLine());
+                        heightPositionCursor = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine(" Установите положение ширины позиции курсора.");
-                            widthPositionCursor = Convert.ToInt32(Console.ReadLine());
+                        widthPositionCursor = Convert.ToInt32(Console.ReadLine());
                         Console.SetCursorPosition(widthPositionCursor, heightPositionCursor);
                         break;
-                    case "6":
-                        Console.WriteLine("Введите слово " + wordClear + " для очистки консоли.");
-                            clearText = Console.ReadLine();
-
-                        if (clearText == wordClear)
-                        {
-                            Console.Clear();
-                        }
+                    case CommandClearConsole:
+                        Console.Clear();
                         break;
-                    case "7":
-                        Console.WriteLine("Введите слово " + wordExit + " для выхода из приложенния.");
-                        exitText = Console.ReadLine();
+                    case CommandExitProgram:
+                        Console.WriteLine("Введите слово " + CommandExitProgram + " для выхода из приложенния.");
 
-                        if (wordExit == exitText)
+                        if (CommandExitProgram == Console.ReadLine())
                         {
                             isExit = true;
                         }
                         break;
-                }    
+                }
             }
         }
     }
