1d048cda14d26267ab012bf42c061a4f75387a3f
VolVla <98841869+VolVla@users.noreply.github.com>
2023-11-10 22:39:51 +0300

    Update RectangleName.cs

--- RectangleName.cs
+++ RectangleName.cs
@@ -6,28 +6,42 @@ namespace RectangleName
     {
         static void Main()
         {
-            int lineSymbol = 0; 
+            int currentLine = 1;
             char symbol;
             string userName;
             string lineUserName;
-            
+            int amountSymbols = 0;
+            int numberRows = 3;
+
             Console.WriteLine("Введите имя пользователя");
             userName = Console.ReadLine();
             Console.WriteLine("Введите любой символ");
             symbol = Convert.ToChar(Console.ReadLine());
-            lineUserName = symbol + userName + symbol ;
-            
-            for(int x = lineSymbol; x < lineUserName.Length; x++)
-            {
-                 Console.Write(symbol);
-            }
-            
-            Console.Write("\n"+ lineUserName + "\n");
+            lineUserName = symbol + userName + symbol;
 
-            for (int x = lineSymbol; x < lineUserName.Length; x++)
+            while (currentLine < numberRows)
             {
-                Console.Write(symbol);
+                if (amountSymbols < lineUserName.Length)
+                {
+                    Console.Write(symbol);
+                    amountSymbols++;
+                }
+
+                if (amountSymbols == lineUserName.Length)
+                {
+                    currentLine++;
+
+                    if (currentLine == numberRows)
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine("\n" + lineUserName);
+                    amountSymbols = 0;
+                }
             }
+
+            Console.ReadKey();
         }
     }
 }
