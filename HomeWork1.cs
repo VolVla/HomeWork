3665281fd9a246d1e867639020ca39c3c1a710f0
VolVla <98841869+VolVla@users.noreply.github.com>
2023-11-10 14:13:31 +0300

    Update HomeWork1.cs

--- HomeWork1.cs
+++ HomeWork1.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +7,15 @@ namespace HomeWork1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             string nameUser;
-            string zodiacSign;
+            string signOfZodiac;
             Console.WriteLine("Как вас зовут");
             nameUser = Console.ReadLine();
             Console.WriteLine("Какой ваш знак зодиака");
-            zodiacSign = Console.ReadLine();
-            Console.WriteLine("Вас зовут " + nameUser + ",вам 21 год, вы " + zodiacSign + " работаете на заводе." );
+            signOfZodiac = Console.ReadLine();
+            Console.WriteLine("Вас зовут " + nameUser + ",вам 21 год, вы " + signOfZodiac + " работаете на заводе." );
         }
     }
 }
