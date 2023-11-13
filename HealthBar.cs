e8952a9d74a810eb82abf128ca04a763f95741e7
VolVla <98841869+VolVla@users.noreply.github.com>
2023-11-13 18:19:50 +0300

    Update HealthBar.cs

--- HealthBar.cs
+++ HealthBar.cs
@@ -1,87 +1,84 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HealthBar
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            char emptySymbol = '_';
+            char fillSymbol = '#';
             bool isExit = false;
             string userInput;
             string wordExit = "exit";
-            int positionLine = 0;
-            int health = 4;
-            int maxHealth = 10;
-            int mana = 1;
-            int maxMana = 5;
-            int percentFillBar = 0;
             string healthBar = "HealthBar";
             string manaBar = "ManaBar";
-            int percent = 100;
-            
+            int firstLine = 0;
+            int secondLine = 1;
+            int maxHealth = 10;
+            int maxMana = 5;
+            int percentFillBar;
+
             while (isExit == false)
             {
-                percentFillBar = SetPercentBar(healthBar, health, maxHealth, percent, percentFillBar );
+                percentFillBar = SetPercentBar(healthBar, maxHealth);
                 Console.Clear();
-                DrawBar(percentFillBar, maxHealth, positionLine,'_', '#') ;
-                percentFillBar = SetPercentBar(manaBar, mana, maxMana, percent, percentFillBar);
-                DrawBar(percentFillBar, maxMana, positionLine + 1 , '_','#');
-                Console.WriteLine("\nДля выхода из программы напишите " + wordExit +" для продолжения напишите любой символ");
+                DrawBar(percentFillBar, maxHealth, firstLine, emptySymbol, fillSymbol);
+                percentFillBar = SetPercentBar(manaBar, maxMana);
+                DrawBar(percentFillBar, maxMana, secondLine, emptySymbol, fillSymbol);
+                Console.WriteLine("\nДля выхода из программы напишите " + wordExit + " для продолжения напишите любой символ");
                 userInput = Console.ReadLine();
-                
-                if(userInput == wordExit)
+
+                if (userInput == wordExit)
                 {
                     isExit = true;
                 }
-                
-                Console.Clear(); 
+
+                Console.Clear();
             }
         }
-        
-        static void StandartPositionText()
+
+        static void SetPositionText()
         {
-            Console.SetCursorPosition(0, 6);
+            int indexY = 0;
+            int indexX = 6;
+            Console.SetCursorPosition(indexX, indexY);
         }
-        
-        static int SetPercentBar(string nameBar, int value ,int maxValue,int percent, int percentFillBar)
+
+        static int SetPercentBar(string nameBar, int maxValue)
         {
-            StandartPositionText();
+            int percent = 100;
+            int percentFillBar;
+            SetPositionText();
             Console.WriteLine("Введите % заполнения шкалы " + nameBar);
-            value = Convert.ToInt32(Console.ReadLine());
+            int value = Convert.ToInt32(Console.ReadLine());
             percentFillBar = value * maxValue / percent;
             return percentFillBar;
         }
-        
-        static void DrawBar(int value, int maxValue, int positionX, char emptySymbol = ' ',char fillSymbol = ' '  )
+
+        static void DrawBar(int value, int maxValue, int positionX, char emptySymbol, char fillSymbol)
         {
-            string bar ="";
+            string bar = "[";
+            char rightFrame = ']';
+            int positionY = 0;
 
             if (value >= 0)
             {
-                for (int i = 0; i < value; i++)
+                for (int i = 0; i < maxValue; i++)
                 {
-                    bar += fillSymbol;
+                    if (i < value)
+                    {
+                        bar += fillSymbol;
+                    }
+                    else
+                    {
+                        bar += emptySymbol;
+                    }
                 }
-            }
 
-            Console.SetCursorPosition(0, positionX);
-            Console.Write('[');
-            Console.Write(bar);
-            bar ="";
-
-            if (value <= maxValue)
-            {
-                for(int i = 0; i < maxValue - value; i++)
-                {   
-                    bar += emptySymbol;
-                }
+                Console.SetCursorPosition(positionY, positionX);
+                Console.Write(bar + rightFrame);
             }
-            
-            Console.Write(bar + "]");
         }
     }
 }
