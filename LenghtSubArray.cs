23ae724c8c3f72a5d80098b1a4b8ddae0f4f55f2
VolVla <98841869+VolVla@users.noreply.github.com>
2023-11-13 15:41:46 +0300

    Update LenghtSubArray.cs

--- LenghtSubArray.cs
+++ LenghtSubArray.cs
@@ -1,44 +1,42 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LenghtSubArray
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             const int AmountNumbers = 30;
-            int[] arrayNumbers = new int[AmountNumbers] { 2, 3, 2, 2, 5, 2, 2, 2, 7, 7, 7, 7, 7, 7, 2, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 2, 2, 3, 3};
+
+            int[] arrayNumbers = new int[AmountNumbers] { 2, 3, 2, 2, 5, 2, 2, 2, 7, 7, 7, 7, 7, 7, 2, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 2, 2, 3, 3 };
             int maximumValue = arrayNumbers[0];
             int maximumValueRepeat = 0;
             int numberRepeat = 1;
 
-           for(int i = 0; i < arrayNumbers.Length - 1; i++)
-           {
-                if(arrayNumbers[i] == arrayNumbers[i + 1])
+            for (int i = 0; i < arrayNumbers.Length - 1; i++)
+            {
+                if (arrayNumbers[i] == arrayNumbers[i + 1])
                 {
                     numberRepeat++;
+
                     if (numberRepeat > maximumValueRepeat)
                     {
                         maximumValueRepeat = numberRepeat;
                         maximumValue = arrayNumbers[i];
-                    }  
+                    }
                 }
-                else if (arrayNumbers[i] != arrayNumbers[i + 1])
+                else
                 {
                     numberRepeat = 1;
                 }
-           }
-            
-           Console.WriteLine($"\nСамое большое число {maximumValue}, кол-во повторений {maximumValueRepeat}");
+            }
 
-           foreach (int value in arrayNumbers)
-           {
-               Console.Write($"{value} ");
-           } 
+            Console.WriteLine($"\nСамое большое число {maximumValue}, кол-во повторений {maximumValueRepeat}");
+
+            foreach (int value in arrayNumbers)
+            {
+                Console.Write($"{value} ");
+            }
         }
     }
 }
