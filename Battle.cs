d121eeaadc9b9407bc1e8b7f76dc498d7c720d19
VolVla <98841869+VolVla@users.noreply.github.com>
2023-11-14 17:00:26 +0300

    Update Battle.cs

--- Battle.cs
+++ Battle.cs
@@ -13,35 +13,22 @@ namespace BattleRpg
 
             string userInput;
             Random random = new Random();
-            bool singleHelpOfSpirits = true;
+            bool canSingleHelpOfSpirits = true;
             int healthHero = 120;
             int healthEnemy = 200;
             int currentValueIsCurse = 0;
             int storageMagicPointVoodoo = 0;
             int pointsFireEffect = 0;
             int minimumEnemyDamage = 30;
-            int minimumDamageSecondSpell = 30;
-            int minimumDamageFirstSpell = 20;
-            int minimumDamageThirdSpell = 70;
-            int minimumDamageFire = 30;
             int maximumEnemyDamage = 45;
-            int maximumDamageSecondSpell = 40;
-            int maximumDamageFirstSpell = 30;
-            int maximumDamageFire = 40;
-            int maximumDamageThirdSpell = 85;
             int pointsPowerCurseOnHero = 1;
-            int fireEffect = 0;
             int conditionApplicationMagicVoodooPoints = 4;
-            int conditionApplicationFirePoints = 3;
             int pointsBeforeDeathfHero = 13;
             int pointsBeforeDeathPerson = 0;
             int enemyDamage = random.Next(minimumEnemyDamage, maximumEnemyDamage);
-            int damegeFromSecondSpell = random.Next(minimumDamageSecondSpell, maximumDamageSecondSpell);
-            int damegeFromFirstSpell = random.Next(minimumDamageFirstSpell, maximumDamageFirstSpell);
-            int damageFire = random.Next(minimumDamageFire, maximumDamageFire);
-            int damegeFromThirdSpell = random.Next(minimumDamageThirdSpell, maximumDamageThirdSpell);
 
-            Console.WriteLine("Вы открыли дверь и встретили босса, он настроен враждебно, чтобы его победить используйте свои способности\nСражение начинается");
+            Console.WriteLine("Вы открыли дверь и встретили босса, он настроен враждебно, чтобы его победить используйте свои способности" +
+            "\nСражение начинается");
 
             while (healthHero >= pointsBeforeDeathPerson & healthEnemy >= pointsBeforeDeathPerson)
             {
@@ -53,22 +40,32 @@ namespace BattleRpg
                 $" игнорирует одну атаку противника,\n и востанавливает не много здоровья \n {CommandCurseEnemy}" +
                 $"- Проклятье куклы вуды (востанавливает не много здоровья на кладывает на противника одно" +
                 $" проклятье вуду.)\n {CommandAttackCurse} - Проткнуть куклу вуду(наносит сильный урон при этом " +
-                $"тратит " + conditionApplicationMagicVoodooPoints + " очка проклятья вуду.)");
+                $"тратит  {conditionApplicationMagicVoodooPoints}  очка проклятья вуду.)");
                 userInput = Console.ReadLine();
 
                 if (currentValueIsCurse == pointsBeforeDeathfHero)
                 {
-                    Console.WriteLine("Вы не успели победить противника.На вас наложилось не изличимое проклятие уродства.\n Вы посмотрели в зеркало и не переживаете этого, и умираете от сердечного приступа.");
+                    Console.WriteLine("Вы не успели победить противника.На вас наложилось не изличимое проклятие уродства." +
+                    "\n Вы посмотрели в зеркало и не переживаете этого, и умираете от сердечного приступа.");
                     healthHero -= healthHero;
                 }
 
                 switch (userInput)
                 {
                     case CommandAttackFireball:
-                        Console.WriteLine("Вы атаковали fireball  и нанесли " + damegeFromFirstSpell + " Враг получил " + pointsFireEffect + " point эффекта подгорания. ");
+                        int minimumDamageFirstSpell = 20;
+                        int minimumDamageFire = 30;
+                        int maximumDamageFirstSpell = 30;
+                        int maximumDamageFire = 40;
+                        int fireEffect = 0;
+                        int conditionApplicationFirePoints = 3;
+                        int damageFire = random.Next(minimumDamageFire, maximumDamageFire);
+                        int damegeFromFirstSpell = random.Next(minimumDamageFirstSpell, maximumDamageFirstSpell);
+
+                        Console.WriteLine($"Вы атаковали fireball  и нанесли   {damegeFromFirstSpell}   Враг получил   {pointsFireEffect}   point эффекта подгорания. ");
                         healthEnemy -= damegeFromFirstSpell;
                         ++pointsFireEffect;
-                        Console.WriteLine("Враг вас атаковал нас и нанес " + enemyDamage + " урона и наложили " + pointsPowerCurseOnHero + " очко проклятья.");
+                        Console.WriteLine($"Враг вас атаковал нас и нанес   {enemyDamage}  урона и наложили {pointsPowerCurseOnHero} очко проклятья.");
                         healthHero -= enemyDamage;
                         ++currentValueIsCurse;
 
@@ -77,43 +74,54 @@ namespace BattleRpg
                             healthEnemy -= damageFire;
                             healthHero += damageFire;
                             pointsFireEffect = fireEffect;
-                            Console.WriteLine("\nВраг подгорел и получил " + damageFire + " урона и запах жареным бекконом это вам востановило " + damageFire + " здоровья.");
+                            Console.WriteLine($"\nВраг подгорел и получил  {damageFire} урона и запах жареным бекконом это вам востановило {damageFire} здоровья.");
                         }
                         break;
+
                     case CommandAttackSpirit:
-                        if (singleHelpOfSpirits == true)
+                        if (canSingleHelpOfSpirits == true)
                         {
-                            Console.WriteLine("Враг попытался нанести вам " + enemyDamage + " урон, но вы призвали духов и они защитили вас и не много подлечили на " + enemyDamage + " здоровья.");
+                            Console.WriteLine($"Враг попытался нанести вам {enemyDamage} урон, но вы призвали духов и они защитили вас и не много подлечили на {enemyDamage} здоровья.");
                             healthHero += enemyDamage;
-                            singleHelpOfSpirits = false;
+                            canSingleHelpOfSpirits = false;
                         }
-                        else if (singleHelpOfSpirits == false)
+                        else if (canSingleHelpOfSpirits == false)
                         {
                             Console.WriteLine("Вы уже использовали эту споспобность.");
                         }
                         break;
+
                     case CommandCurseEnemy:
-                        Console.WriteLine("Вы посылаете на босса проклятье и востанавливаете " + damegeFromSecondSpell);
+                        int minimumDamageSecondSpell = 30;
+                        int maximumDamageSecondSpell = 40;
+                        int damegeFromSecondSpell = random.Next(minimumDamageSecondSpell, maximumDamageSecondSpell);
+
+                        Console.WriteLine($"Вы посылаете на босса проклятье и востанавливаете  {damegeFromSecondSpell}");
                         healthHero += damegeFromSecondSpell;
                         ++storageMagicPointVoodoo;
-                        Console.WriteLine("Враг вас атаковал нас и нанес " + enemyDamage + " урона и наложили " + pointsPowerCurseOnHero + " очко проклятья.");
+                        Console.WriteLine($"Враг вас атаковал нас и нанес  {enemyDamage}  урона и наложили {pointsPowerCurseOnHero} очко проклятья.");
                         healthHero -= enemyDamage;
                         ++currentValueIsCurse;
                         break;
+
                     case CommandAttackCurse:
+                        int minimumDamageThirdSpell = 70;
+                        int maximumDamageThirdSpell = 85;
+                        int damegeFromThirdSpell = random.Next(minimumDamageThirdSpell, maximumDamageThirdSpell);
 
                         if (conditionApplicationMagicVoodooPoints <= storageMagicPointVoodoo)
                         {
-                            Console.WriteLine("\nВы использовали куклу вуду, врагу очень больно вы нанесли " + damegeFromThirdSpell + " урона");
+                            Console.WriteLine($"\nВы использовали куклу вуду, врагу очень больно вы нанесли {damegeFromThirdSpell} урона");
                             healthEnemy -= damegeFromThirdSpell;
-                            Console.WriteLine("Враг вас атаковал нас и нанес " + enemyDamage + " урона и наложили " + pointsPowerCurseOnHero + " очко проклятья.");
+                            Console.WriteLine($"Враг вас атаковал нас и нанес {enemyDamage} урона и наложили  {pointsPowerCurseOnHero} очко проклятья.");
                             healthHero -= enemyDamage;
                             ++currentValueIsCurse;
                             storageMagicPointVoodoo -= conditionApplicationMagicVoodooPoints;
                         }
                         else
                         {
-                            Console.WriteLine("\nВы не можете использовать куклу вуду у вас не достаточно " + (conditionApplicationMagicVoodooPoints - storageMagicPointVoodoo) + " очков магии вуду");
+                            int shortageMagic = conditionApplicationMagicVoodooPoints - storageMagicPointVoodoo;
+                            Console.WriteLine($"\nВы не можете использовать куклу вуду у вас не достаточно {shortageMagic} очков магии вуду");
                         }
                         break;
                 }
@@ -122,12 +130,10 @@ namespace BattleRpg
             if (healthHero <= pointsBeforeDeathPerson && healthEnemy <= pointsBeforeDeathPerson)
             {
                 Console.WriteLine("\nПоздравляю вы убили друг друга. Вы не победили и не проиграли. Досвидания ");
-
             }
             else if (healthHero <= pointsBeforeDeathPerson)
             {
                 Console.WriteLine("\nВас убили.Босс на смехается над вашим трупом.");
-
             }
             else if (healthEnemy <= pointsBeforeDeathPerson)
             {
