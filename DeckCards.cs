a108e26eb7e4b736a4731d7f126db69c35051a88
VolVla <98841869+VolVla@users.noreply.github.com>
2023-11-30 20:17:02 +0300

    Update DeckCards.cs

--- DeckCards.cs
+++ DeckCards.cs
@@ -23,7 +23,7 @@ namespace DeckCards
                 switch (userInput)
                 {
                     case TakeCardCommand:
-                        player.TakeCard(deck._allCards);
+                        player.TakeCards(deck.GetCards());
                         break;
                     case ShowCardHandCommand:
                         player.ShowCards();
@@ -41,7 +41,7 @@ namespace DeckCards
 
     class Deck
     {
-        public List<Card> _allCards = new List<Card>()
+        private List<Card> _allCards = new List<Card>()
         {
             new Card(0,"Камень"),new Card(1,"Ножниц"), new Card(2,"Бумага"),
             new Card(3,"Колодец"), new Card(4,"Пики"), new Card(5,"Крести"),
@@ -50,7 +50,12 @@ namespace DeckCards
             new Card(12,"Самолет"),new Card(13,"Носок"),new Card(14,"Шляпа"),
             new Card(15,"Собака"),new Card(16,"Стол"),new Card(17,"Кружка"),
             new Card(18,"Шторы"),new Card(19,"Чашка"),new Card(20,"Велосипед")
-        } ;
+        };
+
+        public List<Card> GetCards()
+        {
+            return _allCards;
+        }
     }
 
     class Player
@@ -58,13 +63,13 @@ namespace DeckCards
         private List<Card> _deckHand = new List<Card>();
         private Random _random = new Random();
 
-        public void TakeCard(List<Card> _allCards)
+        public void TakeCards(List<Card> allCards)
         {
-            if (_allCards.Count != 0)
+            if (allCards.Count != 0)
             {
-                int randomNumber = _random.Next(0, _allCards.Count);
-                _deckHand.Add(_allCards[randomNumber]);
-                _allCards.Remove(_allCards[randomNumber]);
+                int randomNumber = _random.Next(0, allCards.Count);
+                _deckHand.Add(allCards[randomNumber]);
+                allCards.Remove(allCards[randomNumber]);
             }
             else
             {
@@ -90,13 +95,13 @@ namespace DeckCards
 
     class Card
     {
-        public string Name { get; private set; }
-        public int Value { get; private set; }
-
         public Card(int number, string name)
         {
             Name = name;
             Value = number;
         }
+
+        public string Name { get; private set; }
+        public int Value { get; private set; }
     }
 }
